15d72cc54292864faf5fe0712a2c187716a7d3ed
naqeebasif <naqeebasif@gmail.com>
2019-08-03 17:51:37 +0100

    Modifying sqoop setup file

--- sqoop/setup.sh
+++ sqoop/setup.sh
@@ -22,18 +22,18 @@ fi
 #-------------------------------------------------------------------------------
 #------------------Extracting the tar file--------------------------------------
 
-echo $PASS | sudo tar -xvzf binaries/sqoop-1.4.7.bin__hadoop-2.6.0.tar.gz -C /usr/lib/
-echo $PASS | sudo mv /usr/lib/sqoop-1.4.7.bin__hadoop-2.6.0/ /usr/lib/sqoop
+echo $PASS | sudo tar -xvzf binaries/sqoop-1.4.7.bin__hadoop-2.6.0.tar.gz -C /usr/local/
+echo $PASS | sudo mv /usr/local/sqoop-1.4.7.bin__hadoop-2.6.0/ /usr/local/sqoop
 #-------------------------------------------------------------------------------
 #------------------Configuring bash ---------------------------------------
 echo "" >> $HDUSER_HOME/.bashrc
 echo "#Set SQOOP_HOME" >> $HDUSER_HOME/.bashrc
-echo "export SQOOP_HOME=/usr/lib/sqoop" >> $HDUSER_HOME/.bashrc
+echo "export SQOOP_HOME=/usr/local/sqoop" >> $HDUSER_HOME/.bashrc
 echo "export PATH=\$PATH:\$SQOOP_HOME/bin" >> $HDUSER_HOME/.bashrc
 #---------------------------------------------------------------------
 #------------------Configuring Sqoop----------------------------------------
-sudo cp configs/sqoop-env.sh /usr/lib/sqoop/conf/
-sudo rm mv /usr/lib/sqoop/conf/sqoop-env-template.sh
+sudo cp configs/sqoop-env.sh /usr/local/sqoop/conf/
+sudo rm mv /usr/local/sqoop/conf/sqoop-env-template.sh
 #-------------------------------------------------------------------------
 #----------------------Checking sqoop version ------------------------------
 #sourcing the bash file
@@ -48,9 +48,9 @@ else
   wget wget http://www.mirrorservice.org/sites/ftp.mysql.com/Downloads/Connector-J/mysql-connector-java-5.1.47.tar.gz \
   -P binaries/
 fi
-echo $PASS | sudo tar -xvzf binaries/mysql-connector-java-5.1.47.tar.gz -C /usr/lib/sqoop/lib
-echo $PASS | sudo mv /usr/lib/sqoop/lib/mysql-connector-java-5.1.47/mysql-connector-java-5.1.47-bin.jar \
-/usr/lib/sqoop/lib
+echo $PASS | sudo tar -xvzf binaries/mysql-connector-java-5.1.47.tar.gz -C /usr/local/sqoop/lib
+echo $PASS | sudo mv /usr/local/sqoop/lib/mysql-connector-java-5.1.47/mysql-connector-java-5.1.47-bin.jar \
+/usr/local/sqoop/lib
 #----------------------------------------------------------------------------------
 #--------------------------Importing sample----------------------------------------
 #sqoop-import-all-tables --connect jdbc:mysql://localhost/employees --username root --password hadoop -m 1
